00ab15a19f284f9d1e7d3131a4a6d57edc6aa6c3
42kharuya <haruya.0411.k@gmail.com>
2025-10-29 19:45:13 +0900

    done ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef _FIXED_H_
+#define _FIXED_H_
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+	 int _rawBits;
+	 static const int _FRACTIONAL_BITS = 8;
+
+	public:
+	 Fixed();
+	 ~Fixed();
+	 Fixed(const Fixed& other);
+	 Fixed& operator=(const Fixed& other);
+
+	 int getRawBits (void) const;
+	 void setRawBits (const int raw);
+};
+
+#endif // _FIXED_H_
