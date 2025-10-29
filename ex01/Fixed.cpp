7e4a2580f16546d60055769fcc7c69af1f6c3a5b
42kharuya <haruya.0411.k@gmail.com>
2025-10-29 21:08:40 +0900

    Fixed: Refactor Fixed class constructors and update Makefile for ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -4,6 +4,24 @@ Fixed::Fixed() : _rawBits(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const int &raw) {
+	std::cout << "Default constructor called" << std::endl;
+	_rawBits = num << _kFractionalBits;
+}
+
+static int round_to_int(float f) {
+    if (f >= 0.0f) {
+        return static_cast<int>(std::floor(f + 0.5f));
+    } else {
+        return static_cast<int>(std::ceil(f - 0.5f));
+    }
+}
+
+Fixed::Fixed(const float &raw) {
+	std::cout << "Default constructor called" << std::endl;
+	_rawBits = round_to_int(raw * (1 << _numOfFractionalBits));
+}
+
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -2,15 +2,18 @@
 #define _FIXED_H_
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
 	private:
 	 int _rawBits;
-	 static const int kFractionalBits = 8;
+	 static const int _kFractionalBits = 8;
 
 	public:
 	 Fixed();
+	 Fixed(const int &raw);
+	 Fixed(const float &raw);
 	 ~Fixed();
 	 Fixed(const Fixed &other);
 	 Fixed& operator=(const Fixed &other);
